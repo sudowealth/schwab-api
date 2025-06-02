548e2bc42e752691cd2f38089d4b7971fbd853e1
Daniel Yeoman <dyeoman2@gmail.com>
2025-06-02 11:18:30 -0700

    feat: add overloads for mergeShapes function to support multiple shape combinations
    - Introduced new overloads for the `mergeShapes` function, allowing it to accept two or three ZodRawShape parameters, enhancing flexibility in schema merging.
    - Improved type safety by ensuring the return type accurately reflects the merged shapes.

--- src/utils/schema-utils.ts
+++ src/utils/schema-utils.ts
@@ -1,5 +1,14 @@
 import { type z } from 'zod'
 
+export function mergeShapes<T extends z.ZodRawShape, U extends z.ZodRawShape>(
+	shape1: T,
+	shape2: U,
+): T & U
+export function mergeShapes<
+	T extends z.ZodRawShape,
+	U extends z.ZodRawShape,
+	V extends z.ZodRawShape,
+>(shape1: T, shape2: U, shape3: V): T & U & V
 export function mergeShapes<T extends z.ZodRawShape[]>(
 	...shapes: T
 ): z.ZodRawShape {
